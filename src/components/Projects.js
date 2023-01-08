6778360d0b97425e39ea94dbe2e33f60eab79bc2
Niccatania <Niccatania6@gmail.com>
2023-01-07 22:20:03 -0600

    Mobile responsiveness update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
-import Move from "../assets/img/main_page.png"
+import Move from "../assets/img/move2.png"
 import Pwa from "../assets/img/pwa.png"
 import Dinner from '../assets/img/gif.gif'
 import pyBlog from '../assets/img/pyBLog.png'
@@ -48,21 +48,16 @@ export const Projects = () =>{
     <h5 className="item ">Develup</h5>
   </div>
   
-  
-{/* Project 2 */}
+
+<div className="item hoverLink">
+    <a href="https://owenmg.github.io/Dinner_Decider/"> <img className="imgClassTwo"  src={Dinner} alt="Dinner Decider" /></a>
+    <h5 className="item ">Dinner Decider</h5>
+  </div>
 
 <div className="item hoverLink ">
 <a href="https://pythonblog.herokuapp.com/"> <img className="imgClassTwo " src={pyBlog} alt="python Blog" /> </a>
     <h5 className="item ">Python Blog</h5>
   </div>
-{/* Project 3 */}
-
- 
-  <div className="item hoverLink">
-    <a href="https://owenmg.github.io/Dinner_Decider/"> <img className="imgClassTwo"  src={Dinner} alt="Dinner Decider" /></a>
-    <h5 className="item ">Dinner Decider</h5>
-  </div>
-
             </div>
             </Col>
           </Row>
