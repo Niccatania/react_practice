10c86fdc87c3620a787faaec42d48214e6fe3248
Niccatania <Niccatania6@gmail.com>
2023-02-16 14:58:54 -0600

    portfolio update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import { Container, Row, Col } from "react-bootstrap";
 import Move from "../assets/img/move2.png"
 import Pwa from "../assets/img/pwa.png"
-import Dinner from '../assets/img/gif.gif'
+import Dinner from '../assets/img/dd.png'
 import pyBlog from '../assets/img/pyBLog.png'
 import develup from "../assets/img/develup.png"
 
@@ -50,14 +50,14 @@ export const Projects = () =>{
   
 
 <div className="item hoverLink">
-    <a href="https://owenmg.github.io/Dinner_Decider/"> <img className="imgClassTwo"  src={Dinner} alt="Dinner Decider" /></a>
+    <a href="https://Niccatania.github.io/Dinner_Decider/"> <img className="imgClassTwo"  src={Dinner} alt="Dinner Decider" /></a>
     <h5 className="item ">Dinner Decider</h5>
   </div>
 
-<div className="item hoverLink ">
+{/* <div className="item hoverLink ">
 <a href="https://pythonblog.herokuapp.com/"> <img className="imgClassTwo " src={pyBlog} alt="python Blog" /> </a>
     <h5 className="item ">Python Blog</h5>
-  </div>
+  </div> */}
             </div>
             </Col>
           </Row>
