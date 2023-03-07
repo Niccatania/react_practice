d9af5bf373dc8537b011ad354e05f7f146854b95
Niccatania <Niccatania6@gmail.com>
2023-03-06 23:54:50 -0600

    styling updates, responsive nav scrolling now inverts text

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import Move from "../assets/img/move2.png"
-import Pwa from "../assets/img/pwa.png"
-import Dinner from '../assets/img/dd.png'
-import pyBlog from '../assets/img/pyBLog.png'
+import Dinner from '../assets/img/DDDD.png'
 import develup from "../assets/img/devop.png"
 
 export const Projects = () =>{
@@ -41,17 +39,20 @@ export const Projects = () =>{
               <div className="item hoverLink">
     <a className=""href="https://develup.herokuapp.com/" ><img className="imgClassTwo " src={develup} alt="develup projects" /> </a>
     <h5 className="item ">Develup</h5>
+    <p>A team built Web development service app using React, MongoDB, Graphql, and Chakra UI.</p>
   </div>
   
 
 <div className="item hoverLink">
     <a className=""href="https://move.herokuapp.com/" ><img className="imgClassTwo " src={Move} alt="Move projects" /> </a>
     <h5 className="item ">Move</h5>
+    <p>A team built social blog using handlebars and mySQL</p>
   </div>
 
 <div className="item hoverLink">
     <a href="https://Niccatania.github.io/Dinner_Decider/"> <img className="imgClassTwo"  src={Dinner} alt="Dinner Decider" /></a>
     <h5 className="item ">Dinner Decider</h5>
+    <p>A team built Recipe app uisng HTML, CSS, Javascript and uses API calls.</p>
   </div>
 
 {/* <div className="item hoverLink ">
